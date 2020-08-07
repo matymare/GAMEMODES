54636d5237a413cb63eddedb8d37d4d64dd897b7
matymare <57559400+matymare@users.noreply.github.com>
2020-08-07 17:48:34 +0200

    Edit

--- src/Gamemodes/Main.php
+++ src/Gamemodes/Main.php
@@ -17,26 +17,27 @@ class Main extends PluginBase{
 
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
 		switch($command->getName()){
-		       case "gm":
-		               $sender->sendMessage("§8[§eGAMEMODES§8] §cTry commands: /gms, /gmc, /gmsp, /gma");
-		               break;
-			   case "gms":
-                       $sender->setGamemode(0);
-				       $sender->sendMessage("§8[§eGAMEMODES§8] §aYour game mode has been set to survival!");
-			           break;
-		       case "gmc":
-			           $sender->setGamemode(1);
-			           $sender->sendMessage("§8[§eGAMEMODES§8] §aYour game mode has been set to creative!");
-				       break;
-		       case "gmsp":
-		               $sender->setGamemode(3);
-				       $sender->sendMessage("§8[§eGAMEMODES§8] §aYour game mode has been set to spectator!");
-				       break;
-		       case "gma":
-		               $sender->setGamemode(2);
-				       $sender->sendMessage("§8[§eGAMEMODES§8] §aYour game mode has been set to adventure!");
-				       break;
+		        case "gm":
+		                $sender->sendMessage("§8[§eGAMEMODES§8] §cTry commands: /gms, /gmc, /gmsp, /gma");
+		                break;
+			case "gms":
+                                $sender->setGamemode(0);
+				$sender->sendMessage("§8[§eGAMEMODES§8] §aYour game mode has been set to survival!");
+			        break;
+		        case "gmc":
+			        $sender->setGamemode(1);
+			        $sender->sendMessage("§8[§eGAMEMODES§8] §aYour game mode has been set to creative!");
+				break;
+		        case "gmsp":
+		                $sender->setGamemode(3);
+				$sender->sendMessage("§8[§eGAMEMODES§8] §aYour game mode has been set to spectator!");
+				break;
+		        case "gma":
+		                $sender->setGamemode(2);
+				$sender->sendMessage("§8[§eGAMEMODES§8] §aYour game mode has been set to adventure!");
+				break;
 				}
-			    return true;
+			        return true;
+		
 		}
 }
