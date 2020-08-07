a3a06688d6b6c644304788652125acff186ff708
matymare <57559400+matymare@users.noreply.github.com>
2020-08-07 18:01:42 +0200

    edit

--- src/Gamemodes/Main.php
+++ src/Gamemodes/Main.php
@@ -13,6 +13,7 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 
+
 class Main extends PluginBase{
 
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
