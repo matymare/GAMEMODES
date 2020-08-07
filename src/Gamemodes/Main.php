f6cf261f4f105433c049667c0a42b5b44ce35b04
matymare <57559400+matymare@users.noreply.github.com>
2020-08-07 17:00:59 +0200

    Add new Main.php

--- src/Gamemodes/Main.php
+++ src/Gamemodes/Main.php
@@ -10,23 +10,23 @@ class Main extends PluginBase{
 
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
 		switch($command->getName()){
-		    case "gm":
-		        $sender->sendMessage("§8[§eGAMEMODES§8] §cTry commands: /gms, /gmc, /gmsp, /gma");
-		        break;
+		        case "gm":
+		                $sender->sendMessage("§8[§eGAMEMODES§8] §cTry commands: /gms, /gmc, /gmsp, /gma");
+		                break;
 			case "gms":
-                $sender->setGamemode(0);
+                                $sender->setGamemode(0);
 				$sender->sendMessage("§8[§eGAMEMODES§8] §aYour game mode has been set to survival!");
-			    break;
+			        break;
 			case "gmc":
-			    $sender->setGamemode(1);
+			        $sender->setGamemode(1);
 				$sender->sendMessage("§8[§eGAMEMODES§8] §aYour game mode has been set to creative!");
 				break;
-		    case "gmsp":
-		        $sender->setGamemode(3);
+		        case "gmsp":
+		                $sender->setGamemode(3);
 				$sender->sendMessage("§8[§eGAMEMODES§8] §aYour game mode has been set to spectator!");
 				break;
-		    case "gma":
-		        $sender->setGamemode(2);
+		        case "gma":
+		                $sender->setGamemode(2);
 				$sender->sendMessage("§8[§eGAMEMODES§8] §aYour game mode has been set to adventure!");
 				break;
 				}
